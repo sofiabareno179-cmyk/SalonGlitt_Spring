3c050d4e7090c3aa90ecfce45634afd2eb29fabb
dannakatherinecubidesdiaz-max <dannakatherinecubidesdiaz@gmail.com>
2026-08-21 17:13:11 -0500

    Update CitaRequestDTO.java

--- src/main/java/co/salonglitt/dto/CitaRequestDTO.java
+++ src/main/java/co/salonglitt/dto/CitaRequestDTO.java
@@ -9,6 +9,8 @@ import java.time.LocalDateTime;
 public record CitaRequestDTO(
         @NotNull(message = "El cliente es obligatorio") Long clienteId,
         @NotNull(message = "El servicio es obligatorio") Long servicioId,
-        @NotNull(message = "La fecha y hora son obligatorias") @Future(message = "La cita debe programarse en el futuro") LocalDateTime fechaHora,
-        String estado) {
+        @NotNull(message = "La fecha y hora son obligatorias") 
+        @Future(message = "La cita debe programarse en el futuro") LocalDateTime fechaHora,
+        @NotBlank(message = "El estado es obligatorio") String estado)
+{
 }
