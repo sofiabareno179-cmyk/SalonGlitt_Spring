567551bdcc2d5b00225e776f072c8cdead6d97f0
dannakatherinecubidesdiaz-max <dannakatherinecubidesdiaz@gmail.com>
2026-08-31 19:35:20 -0500

    Create ServicioRepository.java

---
+++ src/main/java/co/salonglitt/repository/ServicioRepository.java
@@ -0,0 +1,7 @@
+package co.salonglitt.repository;
+
+import co.salonglitt.entity.Servicio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicioRepository extends JpaRepository<Servicio, Long> {
+}
