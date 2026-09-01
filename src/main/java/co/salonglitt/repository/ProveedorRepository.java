c2050bd5f95b370ac7e7cb7bd2b76a281ac40f20
dannakatherinecubidesdiaz-max <dannakatherinecubidesdiaz@gmail.com>
2026-08-31 19:35:14 -0500

    Create ProveedorRepository.java

---
+++ src/main/java/co/salonglitt/repository/ProveedorRepository.java
@@ -0,0 +1,7 @@
+package co.salonglitt.repository;
+
+import co.salonglitt.entity.Proveedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProveedorRepository extends JpaRepository<Proveedor, Long> {
+}
